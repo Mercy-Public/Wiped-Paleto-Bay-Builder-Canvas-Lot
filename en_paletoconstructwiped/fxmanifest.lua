96a3961b18f61277a381d4118f65909683f7a8fc
Mercy <161913789+Mercy-Public@users.noreply.github.com>
2024-09-29 09:29:10 -0700

    Add files via upload

---
+++ en_paletoconstructwiped/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Turtle'
+description 'Paleto Construction Wiped'
+version '1.0.0'
+this_is_a_map 'yes'
+-- Created by a certified config dev
+-- Certified config dev of the year, for 1 year
+-- It is irrelevant I was disqualified
